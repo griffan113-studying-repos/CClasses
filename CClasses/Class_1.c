4cd6a919d488cbbf5e393c9cb3bb1fe08cffc952
Eliel <elielmichel@gmail.com>
2022-03-15 18:55:03 -0300

    Adicionar arquivos de projeto.

---
+++ CClasses/Class_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>;
+#include <math.h>
+
+double calculate_circle_area() {
+    const int pi_value = 3.14159;
+
+
+}
+
+int main() {
+    double circle_radius;
+
+    scanf_s("%f", &circle_radius);
+
+    return 0;
+}
